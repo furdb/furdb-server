32604c5b0490535ef807e80e3853cbea373bae63
Madhavan Raja <madhavanraja99@gmail.com>
2022-06-29 23:55:32 +0530

    Input DB info implemented

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,27 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use furdb::{FurColumn, FurDB, FurDBInfo, FurDataType, FurTable, FurTableInfo};
+use std::{path::PathBuf, error::Error};
+
+#[get("/{db}")]
+pub(crate) async fn database(path: web::Path<String>, db_name: String) -> Result<String, Box<dyn Error>> {
+    let db = path.into_inner();
+    let WORKING_DIR = PathBuf::from("D:\\Home\\Repositories\\FurDB\\TestDBs");
+
+    let db_path = WORKING_DIR.clone().push(db);
+    let db_info = FurDBInfo::new(&db_name)?;
+
+    Ok(format!("DB {:?}!", db_path))
+}
+
+#[get("/{db}/{tb}")]
+pub(crate) async fn table(path: web::Path<(String, String)>) -> std::io::Result<String> {
+    let (db, tb) = path.into_inner();
+    Ok(format!("DB {} TB {}!", db, tb))
+}
 
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
-}
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
 }
 
 #[actix_web::main]
@@ -19,8 +29,8 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            .service(database)
+            .service(table)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
