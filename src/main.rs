54db7a4f3b3d8de825597263d9c3ecb8cd0cf8d2
Madhavan Raja <madhavanraja99@gmail.com>
2022-06-30 00:18:03 +0530

    Catching query params correctly

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,30 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use furdb::{FurColumn, FurDB, FurDBInfo, FurDataType, FurTable, FurTableInfo};
-use std::{path::PathBuf, error::Error};
+use std::{error::Error, path::PathBuf};
+
+#[derive(serde::Deserialize)]
+pub(crate) struct DatabaseParams {
+    db_name: String,
+}
 
 #[get("/{db}")]
-pub(crate) async fn database(path: web::Path<String>, db_name: String) -> Result<String, Box<dyn Error>> {
+pub(crate) async fn database(
+    path: web::Path<String>,
+    req: HttpRequest,
+) -> Result<String, Box<dyn Error>> {
     let db = path.into_inner();
-    let WORKING_DIR = PathBuf::from("D:\\Home\\Repositories\\FurDB\\TestDBs");
+    let params = web::Query::<DatabaseParams>::from_query(req.query_string()).unwrap();
+    let working_dir = PathBuf::from("D:\\Home\\Repositories\\FurDB\\TestDBs");
 
-    let db_path = WORKING_DIR.clone().push(db);
-    let db_info = FurDBInfo::new(&db_name)?;
+    let mut db_path = working_dir.clone();
+    db_path.push(db);
 
-    Ok(format!("DB {:?}!", db_path))
+    let db_info = FurDBInfo::new(&params.db_name)?;
+
+    Ok(format!(
+        "DB {:?} | {:?} | {:?}!",
+        db_path, params.db_name, db_info
+    ))
 }
 
 #[get("/{db}/{tb}")]
@@ -26,13 +40,8 @@ async fn hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(database)
-            .service(table)
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    HttpServer::new(|| App::new().service(hello).service(database).service(table))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
