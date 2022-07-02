9a6f1ddbe9e83dc1a0ac82a91b0435c51d7ec332
Madhavan Raja <madhavanraja99@gmail.com>
2022-07-03 00:48:06 +0530

    Removed temporary table get info route

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,6 @@ use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 mod database;
 mod table;
 
-// #[get("/{db}/{tb}")]
-// pub(crate) async fn table(path: web::Path<(String, String)>) -> std::io::Result<String> {
-//     let (db, tb) = path.into_inner();
-//     Ok(format!("DB {} TB {}!", db, tb))
-// }
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
