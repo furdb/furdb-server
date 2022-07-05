dd6c9aa0310b0187eff56d59507834917d3f09a3
Madhavan Raja <madhavanraja99@gmail.com>
2022-07-05 12:39:22 +0530

    Made table get info route's body optional

--- src/table/operations/get_info/mod.rs
+++ src/table/operations/get_info/mod.rs
@@ -11,25 +11,22 @@ mod params;
 use params::TableParams;
 
 mod request;
-use request::TableInfoGenerator;
+use request::TableGenerator;
 
 #[get("/{db}/{tb}")]
 pub(crate) async fn get_info_handler(
     path: web::Path<(String, String)>,
     req: HttpRequest,
-    table_info_generatable: web::Json<TableInfoGenerator>,
+    table_generatable: Option<web::Json<TableGenerator>>,
 ) -> Result<impl Responder, Box<dyn Error>> {
     let (db, tb) = path.into_inner();
     let params = web::Query::<TableParams>::from_query(req.query_string()).unwrap();
 
     let working_dir = params.working_dir.as_ref().map(|wd| PathBuf::from(wd));
-
     let db = get_db(working_dir, &db, params.db_name.clone())?;
 
-    let table_info = table_info_generatable
-        .table_info
-        .as_ref()
-        .map(|table_info_generatable| generate_table_info(table_info_generatable.clone()).unwrap());
+    let table_info = table_generatable
+        .map(|table_generatable| generate_table_info(table_generatable.clone()).unwrap());
 
     let tb = db.get_table(&tb, table_info)?;
 
